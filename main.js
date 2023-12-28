e7e2bbbc393d12f749d490dad0b6e61e0aabcc8d
Donguri2828 <donguri.gamefan@gmail.com>
2023-12-28 13:25:19 +0900

    Treeviewの矩形内にnameとpriceを表示する機能の実装

--- main.js
+++ main.js
@@ -35,21 +35,49 @@ function mapTree(array) {
     const size = (fillFromHeight + i) % 2 === 1
                  ? area * rate / (ctx.canvas.height - whitespace.y)
                  : area * rate / (ctx.canvas.width - whitespace.x);
+    const nameText = String(item.name)
+    const priceText = String(item.price) + '円'
+    const nameTextMetrics = ctx.measureText(nameText)
+    const priceTextMetrics = ctx.measureText(priceText)
 
-    if((fillFromHeight + i) % 2 === 1) {
+    if((fillFromHeight + i) % 2 === 1) { // 縦にfullで埋める処理
+      // 矩形描画
       ctx.fillStyle = '#CBC8C6';
       ctx.fillRect(whitespace.x, whitespace.y, size, ctx.canvas.height - whitespace.y);
       ctx.strokeStyle = '#D44936';
       ctx.lineWidth = 4;
       ctx.strokeRect(whitespace.x, whitespace.y, size, ctx.canvas.height - whitespace.y);
+
+      // テキスト描画
+      const center = { // 矩形の重心
+        x: whitespace.x + size / 2,
+        y: whitespace.y + (ctx.canvas.height - whitespace.y) / 2
+      };
+      ctx.font = '12pt Arial';
+      ctx.fillStyle = '#000000';
+      ctx.fillText(nameText, center.x - (nameTextMetrics.width / 2), center.y);
+      ctx.fillText(priceText, center.x - (priceTextMetrics.width / 2), center.y + 20);
+
       whitespace.x += size;
     }
-    else {
+    else { // 横にfullで埋める処理
+      // 矩形描画
       ctx.fillStyle = '#CBC8C6';
       ctx.fillRect(whitespace.x, whitespace.y, ctx.canvas.width - whitespace.x, size);
       ctx.strokeStyle = '#D44936';
       ctx.lineWidth = 4;
       ctx.strokeRect(whitespace.x, whitespace.y, ctx.canvas.width - whitespace.x, size);
+
+      // テキスト描画
+      const center = { // 矩形の重心
+        x: whitespace.x + (ctx.canvas.width - whitespace.x) / 2,
+        y: whitespace.y + size / 2
+      };
+      ctx.font = '12pt Arial';
+      ctx.fillStyle = '#000000';
+      ctx.fillText(nameText, center.x - (nameTextMetrics.width / 2), center.y);
+      ctx.fillText(priceText, center.x - (priceTextMetrics.width / 2), center.y + 20);
+
       whitespace.y += size;
     }
 
